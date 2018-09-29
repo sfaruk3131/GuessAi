ddb53e9fbc208a4d1e79dfe6c6d64294891633f4
sfaruk3131 <sfaruk3131@bths.edu>
2018-09-28 23:39:56 -0400

    recent update

--- src/guess/AI.java
+++ src/guess/AI.java
@@ -1,4 +1,56 @@
 package guess;
 
+import java.util.Scanner;
+
 public class AI {
+    public static void main(String[] args)
+    {
+        boolean inRange = false;
+        boolean isRight = false;
+        String AskQ = "";
+        String Intro = "";
+        System.out.println("Hello player what is your name!");
+        Scanner name = new Scanner(System.in);
+        String plyname = name.next();
+
+        System.out.println(Intro = "Hello " + plyname + " please input a number ,and I will try to guess it!");
+
+
+        while(inRange == false)
+        {
+
+            Scanner input = new Scanner(System.in);
+            int plynum = input.nextInt();
+            if (plynum >= 1 && plynum <= 100)
+            {
+                inRange = true;
+            }
+            else
+            {
+                System.out.println("Please choose a different input!");
+                inRange = false;
+
+            }
+        }
+
+        int CompGuess = 0;
+
+        System.out.println(plyname + " is your number " + CompGuess);
+
+        Scanner enter = new Scanner(System.in);
+
+        if(enter == CompGuess)
+        {
+            System.out.println("Your number is " + CompGuess);
+        }
+        else
+        {
+            while(isRight == false)
+            {
+                if(enter ==  "higher" )
+                {
+
+                }
+            }
+    }
 }
--- src/guess/main.java
+++ src/guess/main.java
@@ -6,24 +6,31 @@ public class main {
 
     public static void main(String[] args) {
         int numguess = 0;
-        String guesssent = "";
+        String guesssent = "It took ";
+        String tellRng = "";
         boolean isOver = false;
         int CompNum = (int) (Math.random() * 10);
         Scanner name = new Scanner(System.in);
         System.out.println("Hello there player what is your name");
         String Name = name.next();
-        while (isOver == false) {
+        while (isOver == false)
+        {
             Scanner input = new Scanner(System.in);
-            System.out.println("I have a number in my head between  can you guess what it is?");
+            System.out.println(tellRng = "I have a number in my head between 1 and 10 can " + Name + " guess what it is");
             int guess = input.nextInt();
 
 
-            if (guess == CompNum) {
+            if (guess == CompNum)
+            {
                 isOver = true;
                 System.out.println("Congratulations you win!");
+                guesssent = guesssent + Name + " " + numguess + " guesses";
+                System.out.println(guesssent);
 
 
-            } else {
+            }
+            else
+            {
                 System.out.println("Please try again");
                 numguess = numguess + 1;
             }
